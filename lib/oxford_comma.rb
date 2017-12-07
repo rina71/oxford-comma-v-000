1a3342fb5ee2d8fc7e4da8b1bf147f18d64f5997
Yurina Shin <yurinas@gmail.com>
2017-12-07 01:06:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ def oxford_comma(array)
 last_item = array.last
 
 if array.count > 2
-
-array.pop.inspect.join(", ") << " ,and " << last_item
+new_array = [array.pop]
+new_array.join(", ") << " ,and " << last_item
 
 else
   array.join(" and ")
